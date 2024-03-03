7ae5843872bd61c470cd1c44cc788b3d70a5dd60
notHeisenberg007 <mahfuzantor2@gmail.com>
2024-03-03 18:37:00 +0600

    all setup completed

---
+++ js/utility.js
@@ -0,0 +1,36 @@
+function setBackgroundColor(elementID){
+    const elementText = document.getElementById(elementID);
+    elementText.classList.add('bg-[#FFA500]')
+}
+function removeBackgroundColor(elementID){
+    const elementText = document.getElementById(elementID);
+    elementText.classList.remove('bg-[#FFA500]')
+}
+
+
+function setAlphaBate(elementID,value){
+    const elementText = document.getElementById(elementID);
+    elementText.innerText = value;
+}
+
+function getElementValueById(elementID){
+    const getKey = document.getElementById(elementID)
+    const getValue = getKey.innerText;
+    return parseInt(getValue);
+}
+
+function setElementValueById(elementID,value){
+    const setKey = document.getElementById(elementID);
+    setKey.innerText = value;
+}
+
+
+function hideElement(elementID){
+    const ElementText = document.getElementById(elementID);
+    ElementText.classList.add('hidden') 
+}
+
+function showElement(elementID){
+    const ElementText = document.getElementById(elementID);
+    ElementText.classList.remove('hidden') 
+}
